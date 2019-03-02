17491d8fd286d55f859b387b4352113162144732
2419685208@qq.com <2419685208@qq.com>
2019-03-02 22:26:56 +0800

    修改

--- CentralCache.cpp
+++ CentralCache.cpp
@@ -3,26 +3,7 @@
 
 CentralCache CentralCache::_inst;
 
-//// 打桩
-//// 从中心缓存获取一定数量的对象给thread cache
-//size_t CentralCache::FetchRangeObj(void*& start, void*& end, size_t n, size_t bytes)
-//{
-//	start = malloc(bytes * n);
-//	end = (char*)start + bytes*(n-1);
-//
-//	void* cur = start;
-//	while (cur <= end)
-//	{
-//		void* next = (char*)cur + bytes;
-//		NEXT_OBJ(cur) = next;
-//		
-//		cur = next;
-//	}
-//
-//	NEXT_OBJ(end) = nullptr;
-//
-//	return n;
-//}
+
 
 Span* CentralCache::GetOneSpan(SpanList* spanlist, size_t bytes)
 {
